161a7e53c3ce2538b373d1e694b91794299136e9
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-11-28 10:14:38 +0700

    Design-Header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Header from './components/Header';
 import TableUsers from './components/User/TableUsers';
 import { Container } from 'react-bootstrap';
 import { ToastContainer } from 'react-toastify';
+import Home from './components/Home/Home';
+import {
+  Route, Routes
+} from "react-router-dom";
 
 function App() {
-  
+
   return (
     <div className="app-container">
-      <Header />
+      <Header/>
+
       <Container>
-        <TableUsers />
+        <Routes>
+          <Route path="/users" element={<TableUsers />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Container>
       <ToastContainer
         position="top-right"
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,32 +2,49 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Header.scss';
+import logo from '../assets/img/react.png';
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
-      <Container>
-        <Navbar.Brand href="#home">KiMoon</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className='header-container'>
+      <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand >
+            <NavLink className='nav-link' to="/">
+              <img
+                src={logo}
+                width='20'
+                height='20'
+                alt='logo'
+              />
+              <span>KiMoon</span>
+            </NavLink>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" >
+              <NavLink className='nav-link' to="/">Home</NavLink>
+              <NavLink className='nav-link' to="/users">Manage Users</NavLink>
+              
+            </Nav>
+            <Nav>
+                <NavDropdown title="Setting" id="basic-nav-dropdown">
+                  <NavDropdown.Item >
+                    <NavLink className='nav-link' to="/login">Login</NavLink>
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item >
+                    <NavLink className='nav-link' to="logout">Logout</NavLink>
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+
   );
 }
 
